53dc77a7dc043f50b306660ad4de9af0935bc010
PCunh <160776541+PCunh@users.noreply.github.com>
2024-05-15 22:51:57 +0000

    nesta branch implementei uma função que ordena nome e idade

--- main.c
+++ main.c
@@ -1,1 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "ordenar.h"
 
+int main() {
+    int quantidade;
+    printf("Quantas pessoas deseja inserir? ");
+    scanf("%d", &quantidade);
+
+    struct Pessoa *pessoas = malloc(quantidade * sizeof(struct Pessoa));
+    if (pessoas == NULL) {
+        printf("Erro ao alocar memória.\n");
+        return 1;
+    }
+
+    // Solicita ao usuário que insira os dados das pessoas
+    for (int i = 0; i < quantidade; ++i) {
+        printf("Nome da pessoa %d: ", i + 1);
+        scanf("%s", pessoas[i].nome);
+        printf("Idade da pessoa %d: ", i + 1);
+        scanf("%d", &pessoas[i].idade);
+    }
+
+    printf("\nPessoas antes da ordenacao por idade:\n");
+    for (int i = 0; i < quantidade; ++i) {
+        printf("%s, %d anos\n", pessoas[i].nome, pessoas[i].idade);
+    }
+
+    ordenarPorIdade(pessoas, quantidade);
+
+    printf("\nPessoas depois da ordenacao por idade:\n");
+    for (int i = 0; i < quantidade; ++i) {
+        printf("%s, %d anos\n", pessoas[i].nome, pessoas[i].idade);
+    }
+
+    free(pessoas);
+    return 0;
+}
--- ordenar.c
+++ ordenar.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "ordenar.h"
 
+int compararIdade(const void *a, const void *b) {
+    struct Pessoa *pessoaA = (struct Pessoa *)a;
+    struct Pessoa *pessoaB = (struct Pessoa *)b;
+    return (pessoaA->idade - pessoaB->idade);
+}
+
+void ordenarPorIdade(struct Pessoa *pessoas, int tamanho) {
+    qsort(pessoas, tamanho, sizeof(struct Pessoa), compararIdade);
+}
